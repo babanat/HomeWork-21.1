44060b1827eaa6db5bf80c57026aeba7a6cf700d
babanat <babanat3@gmail.com>
2024-09-08 12:00:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Glide('.glide', {
+    type: 'carousel',
+    perView: 1
+    // autoplay: 3000,
+  }).mount();
+});
+
+// Создаем карту и устанавливаем начальные координаты и уровень зума
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+// Добавляем слой карты (tiles)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Добавляем маркер на карту
+var marker = L.marker([51.505, -0.09]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
